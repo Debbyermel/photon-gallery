104d06c66736e5eaa995a4e592e8aa4e1a31b67f
Debora Ermel <debbyermel@gmail.com>
2021-06-25 13:54:28 -0500

    Search images function

--- app.js
+++ app.js
@@ -9,11 +9,11 @@ searchInput.addEventListener('input', updateInput);
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   searchPhotos(searchValue);
-});
+})
 
 function updateInput(e) {
   searchValue = e.target.value;
-}
+};
 
 async function curatedPhotos() {
   const dataFetch = await fetch(URL, {
@@ -34,23 +34,24 @@ async function curatedPhotos() {
   });
 }
 
-// async function searchPhotos(query) {
-//   const searchUrl = `https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page=1`;
-//   const data = await fetch(searchUrl, {
-//     method: 'GET',
-//     headers: {
-//       Accept: 'application/json',
-//       Authorization: auth
-//     }
-//   });
-//    data.photos.forEach(photo => {
-//     const galleryImg = document.createElement('div');
-//     galleryImg.classList.add('gallery-img');
-//     galleryImg.innerHTML = `<img src=${photo.src.large}> </img>
-//     <p>${photo.photographer}</p>
-//     `;
-//     gallery.appendChild(galleryImg);
-//   });
-// }
+  async function searchPhotos(query) {
+    const URLSearch = `https://api.pexels.com/v1/search?query=${query}+qyery&per_page=1`
+    const dataFetch = await fetch(URLSearch, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        Authorization: auth
+      }
+    });
+    const data = await dataFetch.json();
+    data.photos.forEach(photo => {
+      const galleryImg = document.createElement('div');
+      galleryImg.classList.add('gallery-img');
+      galleryImg.innerHTML = `<img src=${photo.src.large}> </img>
+      <p>${photo.photographer}</p>
+      `;
+      gallery.appendChild(galleryImg);
+    });
+  }
 
 curatedPhotos();
